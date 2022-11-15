0c25846fca18e25571311952b470ce48e32f0ec2
milca <milcaph.19@gmail.com>
2022-11-15 17:16:51 -0500

    docs: modify gitignore and install redux

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -18,7 +18,6 @@ const ExpenseItem = (props) => {
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
-        <button>button</button>
       </div>
     </Card>
   );
